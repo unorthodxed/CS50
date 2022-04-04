fbdd51c12a93058e639feb2bd29e08dcb6598877
unorthodxed <unorthodxed@gmail.com>
2022-04-03 20:33:42 -0400

    Snake moves right

--- main.lua
+++ main.lua
@@ -1,10 +1,25 @@
 function love.load()
     timer = 0
+    snakeSegments = {
+        {x = 3, y = 1},
+        {x = 2, y = 1},
+        {x = 1, y = 1}
+    }
 end
-function love.update()
-    timer = timer + 0.15 * dt
-    if timer >= 0.15 then timer = 0 end
+
+function love.update(dt)
+    timer = timer + dt
+    if timer >= 0.15 then 
+        timer = 0
+        local nextPositionX = snakeSegments[1].x + 1
+        local nextPositionY = snakeSegments[1].y 
+
+        table.insert(snakeSegments, 1, {x = nextPositionX,
+        y = nextPositionY})
+        table.remove(snakeSegments)
+    end
 end
+
 function love.draw()
     local gridXcount = 50
     local gridYcount = 35
@@ -18,12 +33,6 @@ function love.draw()
         gridXcount * cellSize,
         gridYcount * cellSize
     )
-
-    local snakeSegments = {
-        {x = 3, y = 1},
-        {x = 2, y = 1},
-        {x = 1, y = 1}
-    }
     for segmentIndex, segment in ipairs(snakeSegments) do
         love.graphics.setColor(.6, 1, .32)
         love.graphics.rectangle(
