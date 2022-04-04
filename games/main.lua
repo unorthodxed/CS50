a95d45a93f8723d85f10b639c074427387433b08
unorthodxed <unorthodxed@gmail.com>
2022-04-04 12:50:31 -0400

    gravity

--- games/main.lua
+++ games/main.lua
@@ -1,7 +1,17 @@
+function love.load()
+    birdY = 200
+    birdYspeed = 0
+end
+
+function love.update(dt)
+    birdY = birdY + (birdYspeed * dt)
+    birdYspeed = birdYspeed + (516 * dt)
+end
+
 function love.draw()
     love.graphics.setColor(.14, .36, .46)
     love.graphics.rectangle('fill', 0, 0, 300, 388)
 
     love.graphics.setColor(.87, .84, .27)
-    love.graphics.rectangle('fill', 62, 200, 30, 25)
+    love.graphics.rectangle('fill', 62, birdY, 30, 25)
 end
