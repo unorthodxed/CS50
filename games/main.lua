7fa5143c0cf7c048323f0d3d54ea915e85fa1c6d
unorthodxed <unorthodxed@gmail.com>
2022-04-04 12:47:18 -0400

    create bird

--- games/main.lua
+++ games/main.lua
@@ -1,4 +1,7 @@
 function love.draw()
     love.graphics.setColor(.14, .36, .46)
     love.graphics.rectangle('fill', 0, 0, 300, 388)
+
+    love.graphics.setColor(.87, .84, .27)
+    love.graphics.rectangle('fill', 62, 200, 30, 25)
 end
