5f9e08f842ae6aac131f087d68390ff9a4b87ee9
unorthodxed <97351274+unorthodxed@users.noreply.github.com>
2022-04-15 16:03:07 -0400

    Update scrabble.c

--- problems-labs/scrabble/scrabble.c
+++ problems-labs/scrabble/scrabble.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-
+//Problem: https://cs50.harvard.edu/x/2022/labs/2/
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 string alphabet = "abcdefghijklmnopqrstuvwxyz";
